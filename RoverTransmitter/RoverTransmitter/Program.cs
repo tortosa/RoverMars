112226965d084a63c65cd0a7beea8f07b687c7ed
Daniel Tortosa <dtortosa@indra.es>
2019-10-16 18:59:53 +0200

    GIT INIT

---
+++ RoverTransmitter/RoverTransmitter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RoverTransmitter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Curiosity!");
+        }
+    }
+}
