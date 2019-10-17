6c480734b7f5332a300985cb02772ce2314a1222
Daniel Tortosa <dtortosa@indra.es>
2019-10-17 15:36:35 +0200

    feature002: conflicts

---
+++ RoverTransmitter/RoverTransmitter/Rover/OrientationEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RoverTransmitter
+{
+    public enum OrientationEnum
+    {
+        North = 0,
+        East = 90,
+        South = 180,
+        West = 270
+    }
+}
